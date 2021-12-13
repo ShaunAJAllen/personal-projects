2841c13eba8c44338cf9a9e8f81f0a22867181bd
ShaunAJAllen <71723993+ShaunAJAllen@users.noreply.github.com>
2021-12-13 16:48:36 +0000

    Uploaded spotify.py and StreamingHistoryML.ipynb

---
+++ spotify.py
@@ -0,0 +1,22 @@
+
+import requests
+
+
+
+def get_access_token(CLIENT_ID, CLIENT_SECRET):
+    AUTH_URL = 'https://accounts.spotify.com/api/token'
+
+    
+
+    auth_response = requests.post(AUTH_URL, {
+        'grant_type' : 'client_credentials',
+        'client_id' : CLIENT_ID,
+        'client_secret' : CLIENT_SECRET,
+    })
+    auth_response_data = auth_response.json()
+
+    access_token = auth_response_data['access_token']
+
+    return access_token
+
+
